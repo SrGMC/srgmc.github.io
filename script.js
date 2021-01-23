3d841f60eac3d4dd554283f4b5af5acea7937866
Álvaro Galisteo <8809764+SrGMC@users.noreply.github.com>
2021-01-23 12:42:41 +0100

    Moved Language toggle logic to script.js

--- script.js
+++ script.js
@@ -1,37 +1,15 @@
-// Hide header
-// if (window.innerWidth < 600) {
-//     window.onscroll = function () {
-//         var header = document.querySelector("header");
-//         if (window.pageYOffset > 80) {
-//             header.classList.remove("fadeInDown");
-//             header.classList.add("fadeOutUp");
-//         }
-//         if (window.pageYOffset < 80 && header.classList.contains("fadeOutUp")) {
-//             header.classList.remove("fadeOutUp");
-//             header.classList.add("fadeInDown");
-//         }
-//     };
-// }
-
-// Show analytics banner
-// if (localStorage.getItem("hideAnalytics") != "true") {
-//     if (window.location.href.includes("/es")) {
-//         console.log("es");
-//         document.querySelector("body").innerHTML +=
-//             '<div class="analytics">¡Hola! Estoy recogiendo datos anónimos para mejorar este sitio web. Puedes revisar en cualquier momento lo que la web recoge en el <a href="/es/transparency">siguiente enlace</a>. <a href="javascript:void" onclick="closeBanner(this);">Cerrar</a>.</div>';
-//     } else {
-//         console.log("en");
-//         document.querySelector("body").innerHTML +=
-//             '<div class="analytics">Hey! I\'m collecting anonymous data to improve this website. You can review at any time what the web collects in <a href="/transparency">this link</a>. <a href="javascript:void" onclick="closeBanner(this);">Close</a>.</div>';
-//     }
-// }
-
 // Language
-// if (navigator.language.toLowerCase().includes("es")) {
-//     window.location.href = "/es";
-// } else {
-//     window.location.href = "/en";
-// }
+var currentLanguage = localStorage.getItem("language");
+currentLanguage = currentLanguage == null ? (navigator.language.toLowerCase().includes("es") ? "es" : "en") : currentLanguage;
+function toggleLanguage(lang) {
+    document.querySelector("html").classList.remove("en");
+    document.querySelector("html").classList.remove(currentLanguage);
+    document.querySelector("html").classList.add(lang);
+    currentLanguage = lang;
+    localStorage.setItem("language", lang);
+}
+
+toggleLanguage(currentLanguage);
 
 // Projects functions
 function fadeBackground() {
@@ -48,9 +26,4 @@ function showIcon(name) {
 
 function hideIcon(name) {
     document.getElementById("header-icon").src = "";
-}
-
-function closeBanner(e) {
-    e.parentElement.style.display = "none";
-    localStorage.setItem("hideAnalytics", "true");
 }
